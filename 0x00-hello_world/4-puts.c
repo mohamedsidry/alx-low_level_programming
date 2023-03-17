28da1e1d4467e0dcb0a4d7afa3391c4050b7b001
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-17 02:12:03 +0100

    betty style correction
    puts function on work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-
 /*
-script that prints "Programming is like building a multilingual puzzle to timinal
+*main-function
+*script that prints
+*"Programming is like building a multilingual puzzle to timinal
 */
+int main(void){
 
-int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
