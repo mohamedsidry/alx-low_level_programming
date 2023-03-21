8bb006b04bf5a6433d8a03aadafc2f3c8e6928d8
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-21 23:43:17 +0000

    Task 6 be positive
    return the pusitive value of given input

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+* _abs - make you positive !) .
+* Return: return positive value of given n.
+* @n : in is the input arg of function .
+*/
+
+
+int _abs(int n)
+{
+int r;
+if (n >= 0)
+{
+r = n;
+}
+else
+{
+r = n * (-1)
+}
+return (r);
+}
