187e597a41c2ae77cbd34f8244f9f25e31afdd9e
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-21 23:30:18 +0000

    currect betty style and print + instade of 1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,19 +3,22 @@
 /**
 * print_sign - checks if given value greater or less or equal to  zero .
 * _putchar - prints gaven character .
-* Return: r = 1 given value is greater than 0 |  r = -1 the given value is less than 0 | r = 0 the given value is equal to 0 .
-*@n: is the input arg of print_sign function . 
+* Return:
+*  r = 1 given value is greater than 0 .
+*  r = -1 the given value is less than 0 .
+*  r = 0 the given value is equal to 0 .
+*@n: is the input arg of print_sign function .
 */
 
 int print_sign(int n)
 {
 int r;
-if(n > 0)
+if (n > 0)
 {
 r = 1;
-_putchar('1');
+_putchar('+');
 }
-else if(n < 0)
+else if (n < 0)
 {
 r = -1;
 _putchar('-');
