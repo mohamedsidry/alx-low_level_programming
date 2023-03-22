7adca62c298af697b3529c2e6b738de575922564
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-22 01:52:31 +0000

    Task 7 !

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+* print_last_digit - print the last digit of given input .
+* _putchar - print the given char .
+* Return: returns the value of the last digit .
+* @n: is the input argument of function
+*/
+
+
+int print_last_digit(int n)
+{
+int r;
+int t;
+int c;
+if (n >= 0)
+{
+t = n;
+}
+else
+{
+t = -n;
+}
+r = t % 10;
+c = (char)r;
+_putchar(c)
+return (r);
+}
