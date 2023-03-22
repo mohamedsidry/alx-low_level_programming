664ca5cbc7ef1502b73ab9ac393c33533e904243
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-22 18:08:27 +0000

    Task 10 Add function
    add 2 input and return the value

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+
+/**
+* add - function take 2 args to add them togather .
+* Return: is the int value of adding the two arguments .
+* @a: is the the first argument .
+* @b: is the second argument .
+*/
+
+
+int add(int a, int b)
+{
+return ((a + b));
+}
