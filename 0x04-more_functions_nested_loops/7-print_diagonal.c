0c7dc5cf2f8482d3543e45c534271decca7a4d96
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-23 17:24:52 +0000

    Betty style

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,31 +3,30 @@
 
 
 /**
-*
-*
-*
-*
+* print_diagonal - prints \ number of given arg .
+* Return: has no return value .
+* @n: is the arg and represent the number of time to repate \ .
+* keep it cool .
 */
 
 
 void print_diagonal(int n)
 {
-  if (n > 0)
-    {
-
-      int i, j;
-      for (i = 0 ; i < n ; i++)
-	{
-	  for(j = 0 ; j < i ; j++)
-	    {
-	      _putchar(' ');
-	    }
-	  _putchar('\\');
-	  _putchar('\n');
-	}
-    }
-  else
-    {
-      _putchar('\n');
-      } 
+if (n > 0)
+{
+int i, j;
+for (i = 0 ; i < n ; i++)
+{
+for (j = 0 ; j < i ; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
