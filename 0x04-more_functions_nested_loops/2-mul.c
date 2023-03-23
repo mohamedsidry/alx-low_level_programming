cd14cf9e4bb9e5353e1d61aa0f235f4823669b91
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-23 15:02:28 +0000

    Task 2
    multiplay tow inputs

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+* mul - multiple tow given argument.
+* @n1: arg[0] argument of index 0.
+* @n2: arg[1] argument of index 1.
+* Return: the value of multiplication of the tow args .
+*/
+
+
+int mul(int n1, int n2)
+{
+return ((n1 * n2));
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define main_h
 int _isupper(int);
 int _isdigit(int);
+int nul(int,int);
 #endif
