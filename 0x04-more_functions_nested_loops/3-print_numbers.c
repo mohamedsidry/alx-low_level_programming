2e34844311b1249d5705bfbdb0ced367818beeb7
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-23 15:17:42 +0000

    Task 3 print numbers
    prints from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+/**
+* print_numbers - prints numbers from 0 to 9 .
+* Return : has no return value .
+*
+*/
+
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10 ; i++)
+{
+_putchar(i);
+}
+_putchar("\n");
+}
