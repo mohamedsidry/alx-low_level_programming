1887390774607b5a20ddf2ee0f1563216b74b03c
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-23 00:43:30 +0000

    Task 100 times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@
 
 void print_times_table(int n)
 {int i, j;
-if (n > 0 && n < 15)
+if (n >= 0 && n < 15)
 {
 for (i = 0 ; i < n + 1; i++)
 {
@@ -22,30 +22,30 @@ _putchar(t + '0');
 }
 else if (t < 10)
 {
+_putchar(',');
+_putchar(' ');
 _putchar(' ');
 _putchar(' ');
 _putchar(t + '0');
 }
 else if (t < 100)
 {
+_putchar(',');
+_putchar(' ');
 _putchar(' ');
 _putchar((t / 10) % 10 + '0');
 _putchar(t % 10 + '0');
 }
 else if (t >= 100)
 {
+_putchar(',');
+_putchar(' ');
 _putchar((t / 100) % 10 + '0');
 _putchar((t / 10) % 10 + '0');
 _putchar(t % 10 + '0');
 }
-if (j < n)
-{
-_putchar(',');
-}
-else
-{
-_putchar(' ');
-}
+
+
 }
 _putchar('\n');
 }
