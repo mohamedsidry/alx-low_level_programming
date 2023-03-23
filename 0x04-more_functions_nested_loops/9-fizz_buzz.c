46ce2c73b3b95b6ab3d4d8018bbe55cb73a3c474
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-23 18:36:58 +0000

    Fuzz Buzz
    remove the space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,26 @@ for (i = 1 ; i < 101 ; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("FizzBuzz ");
+printf(" FizzBuzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf(" Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf(" Buzz");
 }
 else
 {
-printf("%d ", i);
+if (i ==1)
+{
+printf("%d", i);
+}
+else
+{
+printf(" %d", i);
+}
 }
 }
 printf("\n");
