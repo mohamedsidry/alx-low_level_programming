f5b113cc0c48f9fc504a34b1892b90bfc64e05db
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-23 17:02:23 +0000

    Task 7 diagonal
    / times

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+
+/**
+*
+*
+*
+*
+*/
+
+
+void print_diagonal(int n)
+{
+  if (n > 0)
+    {
+
+      int i, j;
+      for (i = 0 ; i < n ; i++)
+	{
+	  for(j = 0 ; j < i ; j++)
+	    {
+	      _putchar(' ');
+	    }
+	  _putchar('/');
+	  _putchar('\n');
+	}
+    }
+  else
+    {
+      _putchar('\n');
+      } 
+}
