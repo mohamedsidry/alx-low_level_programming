09984bc8025d6bb34774d96fa6064696dde9e748
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-24 23:03:56 +0000

    it was currect but not short

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,27 +11,17 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a > b)
-{
-if (a > c)
+if (a > b && a > c)
 {
 largest = a;
 }
-else
-{
-largest = c;
-}
-}
-else if (b > a)
-{
-if (b > c)
+else if (b > c)
 {
 largest = b;
 }
 else
 {
 largest = c;
-}
 }
 return (largest);
 }
