c3da365bbe6ca97d194d7de5be3723ca6173bc18
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-28 00:19:04 +0000

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,26 +3,21 @@
 
 
 /**
-*
-*
+* puts2 - prints other character of a string .
+* @str: input string .
 *
 */
 
 void puts2(char *str)
 {
-  int len = 0;
-  int temp;
-
-  while(str[len])
-
-    {
-      temp = (int)str[len];
-      if(temp % 2 == 0)
-	{
-	  _putchar(temp);
-	}
-      
-      len++;
-    }
-  _putchar('\n');
+int len = 0;
+while (str[len] != '\0')
+{
+if (len % 2 == 0)
+{
+_putchar(str[len]);
+}
+len++;
+}
+_putchar('\n');
 }
