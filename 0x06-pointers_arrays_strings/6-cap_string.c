36567cf7290a655a4f30a1fc268cba718e9a3db1
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-30 18:21:24 +0000

    Task 6 Capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+
+/**
+* cap_string - change lower case character to upper case .
+* @str: pointer to the string .
+* Return: updated Upper case string .
+*
+*/
+
+
+char *cap_string(char *str)
+{
+int len = 0;
+int i;
+ char c[] ={' ', '.', '\t', '\n','{','}',',','(',')',';'};
+while (str[len] != '\0')
+{
+  for (i = 0 ; i < 10 ; i++)
+    {
+      if (str[len] == c[i] && str[len + 1] >= 'a' && str[len + 1] <= 'z')
+	{
+	  str[len + 1] = str[len + 1] - ' ';
+	}
+    }
+len++;
+}
+return (str);
+}
