e72778f730dea9310dfbc3f2ad37e46fa9f2fc43
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-30 00:28:18 +0000

    currect Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,20 +15,39 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
 int len_of_src = 0;
-while (src[len_of_src] != '\0')
+int len_of_dest = 0;
+ 
+while (src[len_of_src] != '\n')
 {
 len_of_src++;
 }
+
+while (dest[len_of_dest] != '\0')
+{
+len_of_dest++;
+}
+ 
 while (src[i] != '\0' && i < n)
 {
 dest[i] = src[i];
 i++;
 }
-while (dest[i] != '\0' && i >= len_of_src )
+
+while (dest[i] != '\0' && i < len_of_src)
 {
-dest[i] = '\0';
+dest[i] = dest[i];
 i++;
 }
-
+/**
+while (dest[i] != '\0' && i > len_of_src && i < len_of_dest)
+{
+  dest[i] = '\0';
+  i++;
+}
+*/
+for(i = len_of_src ; i < len_of_dest ; i++)
+{
+  dest[i] = '\0';
+}
 return (dest);
 }
