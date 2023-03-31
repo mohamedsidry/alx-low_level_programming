9cdcdc8801157f8fed964894d99154b9363223b6
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-03-31 01:47:55 +0000

    currect betty style !

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,50 +3,31 @@
 
 
 /**
-*
-*
-*
+* rot13 - split characters to 2 parts
+* and change char from part 1 with the one in same position in part 2 .
+* same happen with part 2 .
+* @str: input pointer to array of chars.
+* Return: return changed string (like incription ) .
 *
 */
 
 
 char *rot13(char *str)
 {
-  int len_src = 0;
-  int len_a = 0;
-  char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-
-  for (len_src = 0 ; str[len_src] != '\0' ; len_src++)
-
-    {
-      for (len_a = 0 ; a[len_a] != '\0' ; len_a++)
-	{
-	  if(str[len_src] == a[len_a])
-	    {
-	      str[len_src] = b[len_a];
-	      break;
-	    }
-	}
-    }
-
-  /**
-  
-  while(str[len_src] != '0')
-    {
-      while (a[len_a] != '\0')
-	{
-	  if(str[len_src] == a[len_a])
-	    {
-	      str[len_src] = b[len_a];
-	      break;
-	    }
-	  len_a++;
-	}
-      
-      len_src++;
-    }
-  */
-  return (str);
+int len_src = 0;
+int len_a = 0;
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (len_src = 0 ; str[len_src] != '\0' ; len_src++)
+{
+for (len_a = 0 ; a[len_a] != '\0' ; len_a++)
+{
+if (str[len_src] == a[len_a])
+{
+str[len_src] = b[len_a];
+break;
+}
+}
+}
+return (str);
 }
