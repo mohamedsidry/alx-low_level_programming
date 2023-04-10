206e93d9185a5f28fb6f0d889a99124e4f3624a4
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-04-10 23:15:52 +0000

    Task 1
    how many command line argumants i submited ?

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main- the entery point .
+* @argc: the number of argumants .
+* @argv: pointer to an array of command line argumants .
+* Return: the number of argumants .
+*/
+
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+_putchar(argc + '0');
+_putchar('\n');
+return (argc);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,5 @@
 # 0x0A. C - argc, argv
 
 [x] - Task 0 - print the first command line argument ,(index 0).
+
+[x] - Task 1 - print the first number of command line argument .
