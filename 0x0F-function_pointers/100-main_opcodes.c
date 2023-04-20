a08927065897c08d655ce328bf5f7475beed9c0a
cream-vertial-box <mohamed.sidry1999@gmail.com>
2023-04-20 02:46:43 +0000

    currect task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,6 +5,8 @@
 #define ERROR1 1 /** number of argument is not the correct one !*/
 #define ERROR2 2 /** number of bytes is negative !*/
 
+void print_opcode(char *s, int number_of_bytes);
+
 /**
 * main -entry point .
 * @argc: the number of command line args .
@@ -15,7 +17,7 @@
 
 int main(int argc, char *argv[])
 {
-int i, number_of_bytes;
+int number_of_bytes;
 char *arr;
 if (argc != 2)
 {
@@ -28,16 +30,28 @@ if (number_of_bytes < 0)
 printf("Error\n");
 exit(ERROR2);
 }
-arr = (char *)main;
-for (i = 0 ; i < number_of_bytes ; i++)
+print_opcode((char *)&main, number_of_bytes);
+return (0);
+}
+
+/**
+* print_opcode - prints opcode .
+* @fun: poniter to main function .
+* @n: the number of bytes to print .
+*
+*/
+
+
+void print_opcode(char *fun, int n)
 {
-printf("%.2hhx ", arr[i]);
-if (i == number_of_bytes - 1)
+int i;
+for (i = 0 ; i < n ; i++)
+{
+printf("%.2hhx", fun[i]);
+if (i < n - 1)
 {
 printf(" ");
 }
-
 }
 printf("\n");
-return (0);
 }
