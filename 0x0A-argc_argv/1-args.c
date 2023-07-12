cb0a0dc0e89982cf8f0bd585f6690505d7875a95
Mohamed Sidry <mohamed.sidry1999@gmail.com>
2023-07-12 11:21:38 -0700

    Task 1
    prints the arguments count passed to main  !

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - entry point .
+*
+* @argc: number of args passed to main .
+* @argv: array of args passed to main .
+*
+* Return: 0 code is good .
+*/
+
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+printf("%u\n", argc);
+
+return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -3,3 +3,5 @@
 
 
 - [Task 0] : Write a program that prints its name, followed by a new line.
+
+- [Task 1] : Write a program that prints its arguments count, followed by a new line.
