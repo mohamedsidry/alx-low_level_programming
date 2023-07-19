398b98d64388b462ddb8134db66b31c9189996ab
Mohamed Sidry <mohamed.sidry1999@gmail.com>
2023-07-19 10:54:57 -0700

    currect task 0
    add putchar prototype and change header name if any conflect with any header in test !

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,14 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
 
 /*** STANDARD LIBRARIES ***/
 #include <stddef.h>
-
+#include <stdio.h>
 
 /*** FUNCTIONS PROTOTYPES ***/
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
 
-#endif /** MAIN_H  **/
+#endif /** HEADERFILE_H  **/
