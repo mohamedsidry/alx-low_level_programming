e16870559ade4f8f1a1d9c43374b87dbc4790a9e
Mohamed Sidry <mohamed.sidry1999@gmail.com>
2023-07-21 09:27:47 -0700

    Task 0
    sum all given args no matter how many .!

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,13 @@
+# 0x10. C - Variadic functions
+
+- [TASK 0] :  function that returns the sum of all its parameters .
+
+- [] : .
+
+- [] : .
+
+- [] : .
+
+- [] : .
+
+- [] : .
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE_H
+#define HEADER_FILE_H
+
+/*** STANDARD LIBRARIES ***/
+#include <stdarg.h>
+
+/*** SYMBOLIC CONSTENTS  ***/
+
+/*** FUNCTIONS PROTOTYPES  ***/
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif /** HEADER_FILE_H **/
